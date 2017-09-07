726405ff69a45b9f9f9a86f87ec64167eef6e5e0
Alexandre Roux Tekartik <alex@tekartik.com>
2017-09-07 18:04:18 +0200

    feat: add io helper

--- bin/serial_wss_sim.dart
+++ bin/serial_wss_sim.dart
@@ -1,21 +1,5 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:tekartik_serial_wss_client/channel/server/io.dart';
-import 'package:tekartik_serial_wss_sim/serial_wss_sim.dart';
-import 'dart:io';
+import 'package:tekartik_serial_wss_sim/io.dart' as io;
 
 main() async {
-  SerialServer serialServer = await SerialServer.start(ioWebSocketChannelServerFactory);
-  print("started: ${serialServer.deviceInfos}\n[q] [ENTER] to quit");
-  StreamSubscription subscription;
-  subscription = stdin
-      .transform(UTF8.decoder)
-      .transform(new LineSplitter())
-      .listen((data) {
-    if (data == 'q') {
-      print("stopping server...");
-      serialServer.close();
-      subscription.cancel();
-    }
-  });
+  await io.main();
 }
--- bin/serial_wss_sim_dev.dart
+++ bin/serial_wss_sim_dev.dart
@@ -1,23 +1,8 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:tekartik_serial_wss_client/channel/server/io.dart';
 import 'package:tekartik_serial_wss_sim/serial_wss_sim.dart';
-import 'dart:io';
+import 'package:tekartik_serial_wss_sim/io.dart' as io;
 
 main() async {
   // ignore: deprecated_member_use
   SerialServer.debug.on = true;
-  SerialServer serialServer = await SerialServer.start(ioWebSocketChannelServerFactory);
-  print("started: ${serialServer.deviceInfos}\n[q] [ENTER] to quit");
-  StreamSubscription subscription;
-  subscription = stdin
-      .transform(UTF8.decoder)
-      .transform(new LineSplitter())
-      .listen((data) {
-    if (data == 'q') {
-      print("stopping server...");
-      serialServer.close();
-      subscription.cancel();
-    }
-  });
+  await io.main();
 }
